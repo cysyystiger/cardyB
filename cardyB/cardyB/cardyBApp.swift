c3d0b81de37fe2ee50993709947512bbe31c9bd8
Yang-Shan Chen <ychen4@Yang-Shans-MacBook-Pro.local>
2023-10-17 21:56:31 -0700

    first init

---
+++ cardyB/cardyB/cardyBApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardyBApp.swift
+//  cardyB
+//
+//  Created by Yang-Shan Chen on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct cardyBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/api/data', (req, res) => {
+  // Replace this with your data or database query
+  const data = {
+    message: "Hello Node.js!",
+  };
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
